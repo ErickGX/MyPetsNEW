7d8c602c05c13235e3d65e80e2bedea46fbeb10a
ErickGX <ericksaopaulino1@hotmail.com>
2022-04-25 20:25:11 -0300

    Criando ações nos componentes com o click e ligando xml com o java eventos de botoes

--- app/src/main/java/br/com/etecia/mypets/LoginActivity.java
+++ app/src/main/java/br/com/etecia/mypets/LoginActivity.java
@@ -3,12 +3,41 @@ package br.com.etecia.mypets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    //Declarar ps componentes qie serao utilizados nojava
+    //Variaveis globais
+
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar, btnSair;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        //Declarando os componentes no xml para o java
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+
+        //Eventos de clique dos botoes
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+
+    public void acessaJanela(View view) {
     }
 }

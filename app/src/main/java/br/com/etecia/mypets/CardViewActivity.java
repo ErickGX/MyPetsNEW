998e3312b148508e050e52d490a9e266d140ab8d
ErickGX <ericksaopaulino1@hotmail.com>
2022-05-16 19:18:53 -0300

    //criando a varialvel que ira represneta o xml NO java

--- app/src/main/java/br/com/etecia/mypets/CardViewActivity.java
+++ app/src/main/java/br/com/etecia/mypets/CardViewActivity.java
@@ -1,15 +1,33 @@
 package br.com.etecia.mypets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 
 public class CardViewActivity extends AppCompatActivity {
+
+
+    //criando a varialvel que ira represneta o xml NO java
+    CardView idcardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_view_layout);
+
+
+        idcardView = findViewById(R.id.idcardView);
+
+
+        idcardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }

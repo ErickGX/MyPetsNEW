0b98ea3157ef2da33034b9ea5f8a43d4b9af0ba9
ErickGX <ericksaopaulino1@hotmail.com>
2022-05-16 19:31:43 -0300

    //criando a varialvel que ira represneta o xml NO java com varios buttons do carview cafe balistico

--- app/src/main/java/br/com/etecia/mypets/CardViewActivity.java
+++ app/src/main/java/br/com/etecia/mypets/CardViewActivity.java
@@ -5,13 +5,20 @@ import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class CardViewActivity extends AppCompatActivity {
 
 
-    //criando a varialvel que ira represneta o xml NO java
+    //criando a varialvel que ira representa o xml no java
     CardView idcardView;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btnreserva1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,14 @@ public class CardViewActivity extends AppCompatActivity {
         setContentView(R.layout.card_view_layout);
 
 
+
+
         idcardView = findViewById(R.id.idcardView);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btnreserva1 = findViewById(R.id.btnreserva1);
 
 
         idcardView.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +43,43 @@ public class CardViewActivity extends AppCompatActivity {
             }
         });
 
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnreserva1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
+
 }
